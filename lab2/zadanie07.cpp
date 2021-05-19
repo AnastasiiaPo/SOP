e7dd71fa2880e382f2e67f013477461fa884f593
Anastasiia Ponkratova <56721899+s20488@users.noreply.github.com>
2021-05-19 22:46:50 +0200

    Create zadanie07.cpp

---
+++ lab2/zadanie07.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
+
+auto handler_sigusr1(int signum) -> void
+{
+	char msg[] = "Signal ?? SIGUSR1\n";
+	msg[7] = (signum / 10) + '0';
+	msg[8] = (signum % 10) + '0';
+	auto ret = write(2, msg, sizeof(msg) - 1);
+	std::cout << ret;
+}
+auto main() -> int
+{
+	struct sigaction action;
+
+	action.sa_flags = 0;
+	action.sa_handler = handler_sigusr1;
+	sigemptyset(&action.sa_mask);
+
+	sigaction(SIGUSR1, &action, NULL);
+
+	return 0;
+}
