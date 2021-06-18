08fa73e1b1c359b3f9ccd687d47ace15d4138e2a
Anastasiia Ponkratova <56721899+s20488@users.noreply.github.com>
2021-06-18 16:09:43 +0200

    Create zadanie-print-loop.cpp

---
+++ lab6/zadanie-print-loop.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <signal.h>
+
+char buff[64] = {0};
+
+void signalhandler (int x) {
+  auto pathname = "plik.txt";
+  auto a = open(pathname, 0);
+	read(a, buff, 64);
+  
+	close(a);
+}
+
+auto main (int argc, char* argv[]) -> int {
+	auto pathname = "plik.txt";
+  auto a = open(pathname, 0);
+  
+	read(a, buff, 64);
+	close(a);
+  
+	signal(SIGHUP, signalhandler);
+  
+	while(true) {
+		std::cout << buff << "\n";
+		sleep(1);
+	}
+	
+	return 0;
+}
